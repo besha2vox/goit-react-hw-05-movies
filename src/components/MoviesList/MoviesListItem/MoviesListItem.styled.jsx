27a54a4dd41aa856454e6fb66c23d5fe8f6066f6
Serhii Revva <besha2vox@gmail.com>
2023-02-10 21:43:18 +0200

    fix styles

--- src/components/MoviesList/MoviesListItem/MoviesListItem.styled.jsx
+++ src/components/MoviesList/MoviesListItem/MoviesListItem.styled.jsx
@@ -3,7 +3,6 @@ import styled, { css } from 'styled-components';
 const itemStyles = css`
   display: flex;
   flex-direction: column;
-  cursor: pointer;
   background-color: #ffffff;
   transition: box-shadow 300ms ease-in-out, transform 300ms ease-in-out;
   border: 1px solid #2e2e2e70;
@@ -22,15 +21,17 @@ const itemStyles = css`
     justify-content: end;
     flex-grow: 1;
   }
+`;
+
+const Item = styled.li`
+  ${itemStyles}
+
+  cursor: pointer;
 
   &:hover {
     box-shadow: 0 0 3px 2px #db8f2cac;
     transform: scale(1.2);
   }
-`;
-
-const Item = styled.li`
-  ${itemStyles}
 `;
 
 export { itemStyles, Item };
